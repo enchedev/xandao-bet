f0ed7098b11c1a08b4f624c90917c681095bf53d
LeandroLopesM <leandrolopes.tinfem2023@gmail.com>
2025-11-03 07:19:46 -0300

    port de raylib/emscriten/wasm para 3JS/Webgl/JS favor ter paciencia e n mexer nesse folder

---
+++ pages/masmorrasstf/3js/MapMaker.cpp
@@ -0,0 +1,124 @@
+#include <raylib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#define sW 900
+#define sH 900
+#define mH 4
+#define mW 4
+#define tileY (sH / mH)
+#define tileX (sW / mW)
+// struct tile {
+
+// }
+
+bool map[sH][sW][4] = {
+    {
+        {0, 0, 0, 0}
+    }
+};
+
+int main() {
+    InitWindow(sW, sH, "Mapman");
+
+    int x = 0, y = 0;
+    std::string out = "";
+    Font cask = LoadFont("./CASKAYDIAMONONERDFONTMONO-BOLD.TTF");
+
+    while(!WindowShouldClose()) {
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            x = floor(GetMouseX() / (tileX));
+            y = floor(GetMouseY() / (tileY));
+
+            TraceLog(LOG_INFO, "%d / %d = %d", GetMouseX(), sW, x);
+            TraceLog(LOG_INFO, "%d / %d = %d", GetMouseY(), sH, y);
+        }
+
+        if(IsKeyPressed(KEY_UP))    map[x][y][0] = !map[x][y][0];
+        if(IsKeyPressed(KEY_LEFT))  map[x][y][1] = !map[x][y][1];
+        if(IsKeyPressed(KEY_RIGHT)) map[x][y][2] = !map[x][y][2];
+        if(IsKeyPressed(KEY_DOWN))  map[x][y][3] = !map[x][y][3];
+
+        BeginDrawing();
+            ClearBackground(BLACK);
+            for(int i = 0; i < mW; i++) {
+                for(int j = 0; j < mH; j++) {
+                    DrawRectangleLines(
+                        i * tileX, j * tileY, tileX, tileY, WHITE
+                    );
+
+                    if(map[i][j][0])
+                        // out += "^";
+                        DrawLine(
+                            i * tileX / 2,
+                            i * tileY / 2,
+                            i * tileX / 2,
+                            j * tileY / 2 - tileX / 2,
+                            WHITE
+                        );
+                    else if(map[i][j][1])
+                        out += "<";
+                        DrawLine(
+                            i * tileX / 2,
+                            i * tileY / 2,
+                            i * tileX / 2 - tileX / 2,
+                            j * tileY / 2,
+                            WHITE
+                        );
+                    if(map[i][j][2]) 
+                        out += ">";
+                        DrawLine(
+                            i * tileX / 2,
+                            i * tileY / 2,
+                            i * tileX * 2,
+                            j * tileY / 2,
+                            WHITE
+                        );
+                    if(map[i][j][3]) 
+                        out += ".";
+
+                    DrawTextEx(cask, out.c_str(), {(float)i * tileX, (float)j * tileY}, 30, 2, WHITE);
+                    out = "";
+                }
+            }
+            
+            DrawRectangleLines(
+                x * tileX, y * tileY, tileX, tileY, GREEN
+            );
+        EndDrawing();
+    }
+
+    UnloadFont(cask);
+    CloseWindow();
+    
+    // char buffer[4096] = {0};
+
+    // sprintf(buffer, "const map = [");
+    out = "";
+    out = "const map = [\n";
+    for(int i = 0; i < mH; ++i) {
+        out += "\t[\n";
+        // sprintf(buffer, "[");
+// 
+        for(int j = 0; j < mW; ++j) {
+            // sprintf(buffer, "\t[ %d, %d, %d, %d ],\n",
+            //     map[i][j][0], map[i][j][1], map[i][j][2], map[i][j][3]
+            // );
+            out += "\t\t[";
+            out += map[i][j][0]? " 1, " : " 0, ";
+            out += map[i][j][1]? "1, " : "0, ";
+            out += map[i][j][2]? "1, " : "0, ";
+            out += map[i][j][3]? "1 " : "0 ";
+            out += "],\n";
+        }
+
+        // sprintf(buffer, "],\n");
+        out += "\t],\n";
+    }
+    // sprintf(buffer, "]");
+    out += "\n];";
+
+    fflush(stdout);
+    printf("%s", out.c_str());
+    fflush(stdout);
+}
---
+++ pages/masmorrasstf/3js/index.js
@@ -0,0 +1,282 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
+import { degToRad, radToDeg } from 'three/src/math/MathUtils'
+import { InteractionManager } from 'three.interactive'
+import { Tween } from '@tweenjs/tween.js'
+
+let canvas = document.querySelector('#canvas')
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(120, 800 / 450, 0.1, 100)
+// camera.position.z = 2
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(800, 450)
+renderer.domElement.id = 'canvas'
+
+canvas.replaceWith(renderer.domElement)
+const orbit = new OrbitControls(camera, renderer.domElement)
+
+orbit.update()
+
+let intMan = new InteractionManager(renderer, camera, renderer.domElement)
+scene.add(new THREE.GridHelper(10, 10))
+var inMap = false
+
+let animationQ = []
+var turnCooldown = false
+
+let rotationTween = {}
+var rotCopy = [];
+document.addEventListener('keydown', k => {
+    if(k.key === 'm')
+    {
+        inMap = !inMap
+        camera.lookAt(0, 0, 0)
+
+        if(inMap) {
+            rotCopy = [camera.rotation.x, camera.rotation.y, camera.rotation.z];
+            camera.position.y = 3;
+            camera.position.z = -3;
+            orbit.update()
+        }
+        else {
+            camera.rotation.set(...rotCopy)
+            camera.position.set(0, 0, 0)
+        }
+        console.log(inMap)
+    }
+    else if(k.key === 'l')
+        console.log('C ', camera.position.x, ' ', camera.position.z)
+    else if(k.key === 'r')
+        console.log(`Rotation ${0} ${radToDeg(camera.rotation.y)} ${0}`)
+    else if(k.key === 'f')
+        console.log(`facing ${facing()}`)
+    else if(k.code === 'ArrowLeft' && !turnCooldown) {
+        rotationTween = new Tween(camera.rotation)
+            .to({
+                y: degToRad( radToDeg(camera.rotation.y) +90 )
+            }, 500).onComplete(() => turnCooldown = false)
+            .start()
+    }
+    else if(k.code === 'ArrowRight' && !turnCooldown) {
+        turnCooldown = true
+        
+        rotationTween = new Tween(camera.rotation)
+        .to({
+            y: degToRad( radToDeg(camera.rotation.y) -90 )
+        }, 500).onComplete(() => turnCooldown = false)
+            .start()
+        }
+    else if(k.code === 'ArrowUp' && !inMap) {
+        move(facing())
+    }
+    else if(k.code === 'ArrowLeft' && !turnCooldown) {
+        move(inverse(facing()))
+    }
+
+    console.log(k.code)
+})
+
+function facing() {
+    switch(radToDeg(camera.rotation.y)) {
+        case 0:   return Direction.Up
+        case -270:
+        case 90:  return Direction.Left
+        case 270:
+        case -90: return Direction.Right
+        case -180:
+        case 180: return Direction.Down
+    }
+}
+
+const Direction = {
+    Up: 0,
+    Left: 1,
+    Right: 2,
+    Down: 3,
+}
+
+let plane = new THREE.PlaneGeometry(1, 0.75)
+let Walls = [
+    new THREE.Mesh(
+        plane,
+        new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, color: 'white'})
+    ).rotateY(degToRad(180)),
+    new THREE.Mesh(
+        plane,
+        new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, color: 'white'})
+    ).rotateY(degToRad(-90)),
+    new THREE.Mesh(
+        plane,
+        new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, color: 'white'})
+    ).rotateY(degToRad(90)),
+    new THREE.Mesh(
+        plane,
+        new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, color: 'white'})
+    ),
+]
+
+const loader = new THREE.TextureLoader()
+
+let textures = {}
+
+function loadW(f, lbl) {
+    loader.load(
+        f, 
+        text => 
+        {
+            text.colorSpace = THREE.SRGBColorSpace
+            // textures.push(text)
+            if(lbl === 'path' || lbl === 'brick') {
+                for(var i = 0; i < 4; ++i) {
+                    if(map[player.x][player.y][i] === 1 && lbl === 'path') Walls[i].material.map = text
+                    else if(lbl === 'bricks') Walls[i].material.map = text
+
+                    Walls[i].material.needsUpdate = true
+                }
+            }
+
+            textures[lbl] = text
+        }
+        ,
+        ammount =>
+            console.log( fname, 'is ', (ammount.loaded / ammount.total * 100) + '% loaded' )
+        ,
+        // Function called when download errors
+        err => {
+            console.log( 'An error happened: ' )
+            debugger
+            throw(xhr)
+        }
+    )
+}
+    
+loadW('./res/res/path.png', 'path')
+loadW('./res/res/bricks.png', 'bricks')
+
+scene.add(...Walls)
+
+intMan.add(Walls[Direction.Up])
+intMan.add(Walls[Direction.Left])
+intMan.add(Walls[Direction.Right])
+intMan.add(Walls[Direction.Down])
+
+Walls[Direction.Up].position.z = 0.5
+Walls[Direction.Left].position.x = 0.5
+Walls[Direction.Right].position.x = -0.5
+Walls[Direction.Down].position.z = -0.5
+
+function animate() {
+    // requestAnimationFrame(animate)
+    intMan.update()
+    
+    if(radToDeg(camera.rotation.y) % 90 !== 0 && inMap)
+        camera.rotation.y = degToRad(Math.round(radToDeg(camera.rotation.y) / 90) * 90)
+    if(radToDeg(camera.rotation.y) >= 270 || radToDeg(camera.rotation.y) <= -360)
+        camera.rotation.y = 0
+
+    orbit.enabled = inMap
+    Walls.forEach(ea => ea.visible = !inMap)
+    mapBlocks.forEach(ea => ea.visible = inMap)
+    
+    if(rotationTween.update) rotationTween.update()
+    if(zoomTween.update) zoomTween.update()
+    if(inMap) orbit.update()
+    
+    renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate)
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+Walls[Direction.Up].addEventListener('click', ev => move(Direction.Up))
+Walls[Direction.Down].addEventListener('click', ev => move(Direction.Down))
+Walls[Direction.Left].addEventListener('click', ev => move(Direction.Left))
+Walls[Direction.Right].addEventListener('click', ev => move(Direction.Right))
+
+let zoomTween = {}
+function move(dir) {
+    if(!inMap && (dir === undefined || map[player.x][player.y][dir] === 1)) {
+        if(dir) {
+            switch(dir) {
+                case Direction.Up: player.y--; break
+                case Direction.Down: player.y++; break
+                case Direction.Right: player.x++; break
+                case Direction.Left: player.x--; break
+            }
+        }
+        
+        zoomTween = new Tween(camera)
+            .to({fov: 30}, 500)
+            .chain(
+                new Tween(camera)
+                    .to({fov: 90}, 500)
+            ).start()
+        
+        // if not pushed yet.
+        if(mapBlockMirror.find(e => e.x == player.x && e.y == player.y) === undefined) { 
+            const temp = new THREE.Mesh(
+                new THREE.BoxGeometry(1, 1, 1),
+                new THREE.MeshBasicMaterial({ color: 'darkgray'})
+            )
+            temp.position.x = player.y
+            temp.position.z = player.x
+            scene.add(temp)
+            mapBlocks.push(temp)
+            mapBlockMirror.push({x: player.x, y: player.y})
+        }
+
+        for(var i = 0; i < 4; ++i) {
+            if(map[player.x][player.y][i]) Walls[i].material.map = textures['path']
+            else Walls[i].material.map = textures['bricks']
+
+            Walls[i].material.needsUpdate = true
+        }
+    }
+    console.log(player.x, ' ', player.y)
+}
+
+let mapBlocks = []
+let mapBlockMirror = []
+
+const map = [
+    [
+            [ 0, 0, 1, 1 ],
+            [ 1, 0, 1, 1 ],
+            [ 1, 0, 1, 0 ],
+            [ 0, 0, 0, 0 ],
+    ],
+    [
+            [ 0, 1, 0, 1 ],
+            [ 1, 1, 0, 1 ],
+            [ 1, 1, 0, 0 ],
+            [ 0, 0, 0, 0 ],
+    ],
+    [
+            [ 0, 0, 0, 0 ],
+            [ 0, 0, 0, 0 ],
+            [ 0, 0, 0, 0 ],
+            [ 0, 0, 0, 0 ],
+    ],
+    [
+            [ 0, 0, 0, 0 ],
+            [ 0, 0, 0, 0 ],
+            [ 0, 0, 0, 0 ],
+            [ 0, 0, 0, 0 ],
+    ],
+
+];
+
+window.player = {
+    x: 0,
+    y: 0
+}
+move(undefined)
+
+renderer.setAnimationLoop(animate)
+renderer.setClearColor(0x0)
